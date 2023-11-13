7fc8ae32aab6c3bddbc2a6dddf0668ad00491004
Lucas <imcvlucas@gmail.com>
2023-11-13 10:37:09 -0500

    updated requirements.txt

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d1&d0**e3ir@c&l2u+uj66zlw3d*53(61(=tglap66p#gm9#39'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
