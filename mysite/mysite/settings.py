7115b275a05a8930f70a5664371227fd888cbd6a
Lucas <imcvlucas@gmail.com>
2023-11-14 01:41:59 -0500

    added heroku config file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-d1&d0**e3ir@c&l2u+uj66zlw3d*53(61(=tglap66p#gm9#39
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
