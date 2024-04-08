231af6f59c1babc62167037be50d077a1497e70c
Lucas <imcvlucas@gmail.com>
2024-04-08 18:33:54 -0400

    changed URL in profiles.jsx

--- mysite/frontend/src/api/profiles.jsx
+++ mysite/frontend/src/api/profiles.jsx
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
 
+// export default axios.create({
+//     baseURL:'http://localhost:5173/api',
+// })
+
 export default axios.create({
-    baseURL:'http://localhost:8000/api'
+    baseURL:'https://unified-fugitive-database.vercel.app/api/',
 })
